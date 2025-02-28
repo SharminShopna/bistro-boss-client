182971cfacc965675586636b084ec5758f57f734
shopna's Macbook <sharminshopna149@gmail.com>
2025-02-28 23:41:30 +0600

    added banner

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,6 +1,21 @@
 
 
 const NavBar = () => {
+
+    const navOptions = (
+        <>
+            <li>
+                <a>Item 1</a>
+            </li>
+            <li>
+                <a>Item 2</a>
+            </li>
+            <li>
+                <a>Item 3</a>
+            </li>
+
+        </>
+    )
     return (
         <div>
             <div className="navbar bg-base-100">
@@ -23,35 +38,17 @@ const NavBar = () => {
                         <ul
                             tabIndex={0}
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                           {navOptions}
                         </ul>
                     </div>
                     <div className="flex flex-col pl-2">
-                    <h2 className="font-bold text-2xl">BISTRO BOSS</h2>
-                    <p className="">R E S T A U R A N T</p>
+                        <h2 className="font-bold text-2xl">BISTRO BOSS</h2>
+                        <p className="">R E S T A U R A N T</p>
                     </div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {navOptions}
                     </ul>
                 </div>
                 <div className="navbar-end">
